a24541b83a6a64b3a29113dc7d34bf08a226c3ab
Dave Kobrenski <dave@bbmdesigns.com>
2025-03-05 15:02:42 -0500

    initial commit

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,29 @@
-module.exports = () => ({});
+
+module.exports = ({ env }) => ({
+    email: {
+        config: {
+            provider: 'sendgrid',
+            providerOptions: {
+                apiKey: env('SENDGRID_API_KEY'),
+            },
+            settings: {
+                defaultFrom: env('EMAIL_FROM'),
+                defaultReplyTo: env('EMAIL_FROM'),
+                testAddress: env('EMAIL_FROM'),
+            },
+        },
+    },
+    graphql: {
+        config: {
+            endpoint: '/graphql',
+            shadowCRUD: true,
+            playgroundAlways: true, // Disable in production for security
+            depthLimit: 10,
+            amountLimit: 100,
+            apolloServer: {
+                tracing: false,
+                introspection: true, // Disable in production for security
+            },
+        },
+    },
+});
